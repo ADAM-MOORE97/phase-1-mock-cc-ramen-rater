90ef792098b2d3459c380e220265fef2c3f2b96b
Adam <adammoore118@outlook.com>
2021-09-29 12:12:20 -0400

    work so far

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Phase 1 Mock Code Challenge: Ramen Rater
+<!-- # Phase 1 Mock Code Challenge: Ramen Rater
 
 ## Learning Goals
 
@@ -42,7 +42,7 @@ As a user, I can:
 - See all ramen images in the `div` with the id of `ramen-menu`. When the page
   loads, request the data from the server to get all the ramen objects. Then,
   display the image for each of the ramen using an `img` tag inside the
-  `#ramen-menu` div.
+  `#ramen-menu` div. -->
 - Click on an image from the `#ramen-menu` div and see all the info about that
   ramen displayed inside the `#ramen-detail` div and where it says
   `insert comment here` and `insert rating here`.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,79 @@
 // write your code here
+
+
+function getImages(){
+
+fetch('http://localhost:3000/ramens')
+.then(res=>res.json())
+.then(ramens => renderImages(ramens))
+}
+function renderImages(ramens){
+    const div = document.getElementById('ramen-menu')
+    ramens.forEach(ramens=>{
+        const img=document.createElement('img')
+        img.src = ramens.image;
+        img.id = ramens.id
+        img.className = "ramenpics"
+      
+        // img.id = ramens.id
+       img.addEventListener('click', imageClick)
+       div.appendChild(img)
+        
+    })
+
+    
+}
+function imageClick(e){
+    fetch('http://localhost:3000/ramens')
+.then(res=>res.json())
+.then(ramens =>details(ramens));}
+
+function details(ramens){
+    const img = document.querySelector('#1')
+    const h2 = document.getElementsByClassName('name')
+    if(img.src = "./assets/ramen/shoyu.jpg"){
+        h2.textContent = ramens.name
+    }
+    else{
+        console.log('fail')
+    }
+}
+    
+    
+    
+    
+    
+// const div = document.getElementById('ramen-detail')
+// const img=document.querySelector('.ramenpics')
+// let image = document.createElement('img')
+// if(img.id===1){
+    // let image = document.getElementsByClassName('detail-image')
+//     image.src=img.src
+//   console.log(div.appendChild(image))
+    
+
+// else{console.log(failure)}
+// console.log(image.src)
+    // get
+    // if(){
+
+    // }
+    
+
+
+
+
+    
+
+// console.log(renderImages)
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    getImages();
+    imageClick();
+})
